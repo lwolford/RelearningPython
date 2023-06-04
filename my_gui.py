e1bb90c25544e44d7394f99c6ff1db7a5987e104
lwolford <43486632+lwolford@users.noreply.github.com>
2023-06-03 21:32:29 -0400

    Updated UI
    UI now has images, and gives a response message on who has won

--- my_gui.py
+++ my_gui.py
@@ -3,6 +3,7 @@ import time
 import customtkinter as ctk
 import tkinter as tk
 from jankenpo import jankenpo_logic
+from PIL import Image
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("dark-blue")
@@ -22,9 +23,13 @@ class rpsgui:
         self.label = ctk.CTkLabel(master=self.frame, text="Tester")
         self.label.pack(pady=12, padx=10)
 
-        self.rock_button = ctk.CTkButton(master=self.frame, text="Rock", width=100, height=100, command=self.rock)
-        self.paper_button = ctk.CTkButton(master=self.frame, text="Paper", width=100, height=100, command=self.showButtons)
-        self.scissors_button = ctk.CTkButton(master=self.frame, text="Scissors", width=100, height=100, command=self.jankenpo.scissors)
+        rock_image = ctk.CTkImage(light_image=Image.open("Rock.png"), dark_image=Image.open("Rock.png"), size=(100, 100))
+        paper_image = ctk.CTkImage(light_image=Image.open("Notebook Paper.png"), dark_image=Image.open("Notebook Paper.png"), size=(71, 100))
+        scissors_image = ctk.CTkImage(light_image=Image.open("Scissors.png"), dark_image=Image.open("Scissors.png"), size=(100, 100))
+
+        self.rock_button = ctk.CTkButton(master=self.frame, text="", image=rock_image, width=100, height=100, command=self.rock)
+        self.paper_button = ctk.CTkButton(master=self.frame, text="", image=paper_image, width=100, height=100, command=self.paper)
+        self.scissors_button = ctk.CTkButton(master=self.frame, text="", image=scissors_image, width=100, height=100, command=self.scissors)
 
         self.showButtons()
 
@@ -38,9 +43,23 @@ class rpsgui:
         self.paper_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
         self.scissors_button.place(relx=0.75, rely=0.5, anchor=tk.CENTER)
 
+    def gui_reset(self, result):
+        label2 = ctk.CTkLabel(master=self.frame, text=result)
+        label2.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+        self.root.after(3000, label2.place_forget)
+        self.root.after(3000, self.showButtons)
+
     def rock(self):
         self.hideButtons()
-        self.jankenpo.rock()
+        self.gui_reset(self.jankenpo.play_round("rock"))
+
+    def paper(self):
+        self.hideButtons()
+        self.gui_reset(self.jankenpo.play_round("paper"))
+
+    def scissors(self):
+        self.hideButtons()
+        self.gui_reset(self.jankenpo.play_round("scissors"))
 
     def run(self):
         self.root.mainloop()
